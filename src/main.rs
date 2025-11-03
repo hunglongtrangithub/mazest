07f2e8ad07c0d12cbf7251ea9619f196706e864d
Hung Tran <hunglongtran2004@gmail.com>
2025-11-02 20:17:51 -0500

    feat: only log in debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use mazest::app::App;
 
 fn main() -> std::io::Result<()> {
-    // Initialize tracing subscriber for logging
-    let writer = tracing_appender::rolling::never("logs", "app.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(writer);
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .with_writer(non_blocking)
-        .with_line_number(true)
-        .init();
+    #[cfg(debug_assertions)]
+    {
+        // Initialize tracing subscriber for logging
+        let writer = tracing_appender::rolling::never("logs", "app.log");
+        let (non_blocking, _guard) = tracing_appender::non_blocking(writer);
+        tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .with_writer(non_blocking)
+            .with_line_number(true)
+            .init();
+    }
 
     // Set up terminal and run the application
     let mut stdout = std::io::stdout();
